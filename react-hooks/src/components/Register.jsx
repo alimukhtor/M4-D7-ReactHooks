99d709e2dd944d41ed58ef177217042a6585c381
alimukhtor <james1999007@gmail.com>
2021-11-18 22:55:55 +0100

    added validate form to sign up

--- react-hooks/src/components/Register.jsx
+++ react-hooks/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import {Container, Row, Col, Form, Button, Spinner} from 'react-bootstrap'
+import {Container, Row, Col, Form, Button} from 'react-bootstrap'
 import {Component} from 'react'
 class Register extends Component {
   state = {
@@ -37,60 +37,82 @@ class Register extends Component {
     }
   return isItFilled
   }
+
+
+  handleRegistrationform =(e)=> {
+    e.preventDefault()
+    this.setState({
+      errorInput : true
+    })
+  }
 render(){
   return(
-      <Container>
-        <Row className="justify-content-center form-register mt-5">
-          <Col xs={12} md={6}>
-            <h1 className="text-center">Register here !</h1>
-            <Form>
-              <Form.Group>
-              <Form.Label className="font-weight-bold">Name:</Form.Label>
-              <Form.Control type="text" required title="at least 2 chars" placeholder="Enter your name"
-              value={this.state.registration.name}
-              onChange={(e) => this.handleInput('name', e.target.value)}
-              />
-              </Form.Group>
-              <Form.Group>
-              <Form.Label className="font-weight-bold">Surname:</Form.Label>
-              <Form.Control type="text" required title="at least 3 chars" placeholder="Enter your surname"
-              value={this.state.registration.surname}
-              onChange={(e) => this.handleInput('surname', e.target.value)}
-              />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label className="font-weight-bold">Email address:</Form.Label>
-                <Form.Control type="email"   placeholder="alimukhtor@example.com"
-                value={this.state.registration.email}
-                onChange={(e) => this.handleInput('email', e.target.value)}
-                />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label className="font-weight-bold">Password:</Form.Label>
-                  <Form.Control type="password"  required title="Should contain at least 8 chars, 1 digit, 1 letter" placeholder="Password"
-                  value={this.state.registration.password}
-                  onChange={(e) => this.handleInput('password', e.target.value)}
+    <>
+    {
 
+      (this.state.errorInput) ?
+      <h1>Registration completed!</h1> :
+
+      (
+        <>
+        <Container>
+          <Row className="justify-content-center form-register mt-5">
+            <Col xs={12} md={6}>
+              <h1 className="text-center">Register here !</h1>
+              <Form onChange={this.handleRegistrationform}>
+                <Form.Group>
+                <Form.Label className="font-weight-bold">Name:</Form.Label>
+                <Form.Control type="text" required title="at least 2 chars" placeholder="Enter your name"
+                value={this.state.registration.name}
+                onChange={(e) => this.handleInput('name', e.target.value)}
+                />
+                </Form.Group>
+                <Form.Group>
+                <Form.Label className="font-weight-bold">Surname:</Form.Label>
+                <Form.Control type="text" required title="at least 3 chars" placeholder="Enter your surname"
+                value={this.state.registration.surname}
+                onChange={(e) => this.handleInput('surname', e.target.value)}
+                />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label className="font-weight-bold">Email address:</Form.Label>
+                  <Form.Control type="email"   placeholder="alimukhtor@example.com"
+                  value={this.state.registration.email}
+                  onChange={(e) => this.handleInput('email', e.target.value)}
                   />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label className="font-weight-bold">Repeat Password:</Form.Label>
-                <Form.Control type="password" required title="Should match with your password" placeholder="Password"
-                value={this.state.registration.confirmPassword}
-                onChange={(e) => this.handleInput('confirmPassword', e.target.value)}
-                />
-              </Form.Group>
-              <Button variant="success" type="submit" disabled={!this.isClickedBtn()}>
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label className="font-weight-bold">Password:</Form.Label>
+                    <Form.Control type="password"  required title="Should contain at least 8 chars, 1 digit, 1 letter" placeholder="Password"
+                    value={this.state.registration.password}
+                    onChange={(e) => this.handleInput('password', e.target.value)}
 
-                Sign Up
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
+                    />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label className="font-weight-bold">Repeat Password:</Form.Label>
+                  <Form.Control type="password" required title="Should match with your password" placeholder="Password"
+                  value={this.state.registration.confirmPassword}
+                  onChange={(e) => this.handleInput('confirmPassword', e.target.value)}
+                  />
+                </Form.Group>
+                <Button variant="success" type="submit" disabled={!this.isClickedBtn()}>
+
+                  Sign Up
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+        </>
+      )
+
+    }
+    </>
+
     )
   }
 }
